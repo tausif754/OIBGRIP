55205bef1bd7eeeb8ef12eb39e7d1b31c6796da2
tausif754 <115718023+tausif754@users.noreply.github.com>
2023-02-27 00:58:05 +0530

    Temperature Converter
    This is a mini Temperature Converter Project

---
+++ script.js
@@ -0,0 +1,24 @@
+const calculateTemp = () => {
+    const numberTemp = document.getElementById('temp').value;
+    const tempSelected = document.getElementById('temp_diff');
+    const valueTemp = temp_diff.options[tempSelected.selectedIndex].value;
+    const celToFan = (cel) => {
+        let fahrenheit = Math.round((cel * 9 / 5) + 32);
+        return fahrenheit;
+    }
+    const fehToCel = (fehr) => {
+        let celsius = Math.round((fehr - 32) * 5 / 9);
+        return celsius;
+    }
+    let result;
+    if (valueTemp == 'cel') {
+        result = celToFan(numberTemp);
+        document.getElementById('resultContainer').innerHTML = `= ${result} °Fahrenheit`;
+
+    }
+    
+    else {
+        result = fehToCel(numberTemp);
+        document.getElementById('resultContainer').innerHTML = `= ${result} °Celsius`;
+    }
+}
